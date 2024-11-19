e17b6612cd581d37b8896eb98dc9816f1c741d8a
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-11-20 00:31:15 +0600

    JWT Verification and Method Added

--- index.js
+++ index.js
@@ -14,9 +14,38 @@ app.use(
 );
 app.use(express.json());
 
+// Token verification
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res.send({ message: "No Token" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_KEY_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.send({ message: "Invalid Token" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 app.get("/", (req, res) => {
   res.send("Server Started");
 });
+
+// JWT
+app.post("/authentication", async (req, res) => {
+  const userEmail = req.body;
+
+  const token = jwt.sign(userEmail, process.env.ACCESS_KEY_TOKEN, {
+    expiresIn: "5d",
+  });
+  res.send({ token });
+});
+
+
 
 app.listen(port, () => {
   console.log(`${port}PORT`);
